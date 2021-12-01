35f7af318c8f5841acf404f16718cf7543e40a1a
Lasse Letager Hansen <lasse@letager.dk>
2021-12-01 14:27:49 +0100

    Pearlite test added

--- language/tests/1_backend_files.rs
+++ language/tests/1_backend_files.rs
@@ -42,3 +42,10 @@ fn positive_loops() {
 fn positive_types() {
     run_tests("backend-tests/types.rs", HashMap::from([("coq", true)]));
 }
+
+#[test]
+fn positive_pearlite_auction() {
+    run_tests("backend-tests/auction.rs",
+              "Auction",
+              HashMap::from([("coq", true)]));
+}
