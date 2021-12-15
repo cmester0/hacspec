4dda735f541b9911bd3b50231032a156960c2d05
Lasse Letager Hansen <lasse@letager.dk>
2021-12-15 13:18:56 +0100

    wip

--- language/language-tests/option.rs
+++ language/language-tests/option.rs
@@ -8,3 +8,19 @@ pub fn foo(x: Option<Option<U32>>) -> U32 {
         Option::<Option<U32>>::Some(x) => x.unwrap(),
     }
 }
+
+pub type MySingleOptionType =  Option<U32>;
+pub type MyDoubleOptionType =  Option<Option<U32>>;
+
+pub fn bar(x: MyDoubleOptionType) -> U32 {
+    let y = MySingleOptionType::None;
+    let _z = MyDoubleOptionType::Some(y);
+    match x {
+        MyDoubleOptionType::None => U32(0u32),
+        MyDoubleOptionType::Some(x) =>
+            match x {
+                MySingleOptionType::None => U32(0u32),
+                MySingleOptionType::Some(x) => x
+            },
+    }
+}
