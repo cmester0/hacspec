39528f4b89fda7a4c0bb38fd5673c7314ccdfa68
Lasse Letager Hansen <lasse@letager.dk>
2021-12-21 14:09:51 +0100

    Concordium update

--- coq/src/ConcordiumImpls.v
+++ coq/src/ConcordiumImpls.v
@@ -9,7 +9,7 @@ Require Import Hacspec.Lib.
 
 Notation "'reject'" := (int32) : hacspec_scope.
 
-Definition reject_impl_default  : reject :=
+Definition reject_impl_deafult  : reject :=
   min.
 
 Notation "'option_reject'" := ((option reject)) : hacspec_scope.
@@ -24,10 +24,7 @@ Definition reject_impl_convert_from_unit  : reject :=
 Theorem ensures_reject_impl_convert_from_unit : forall result_1 ,
 @reject_impl_convert_from_unit  = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof.
-  intros; subst.
-  reflexivity.
-Qed.
+Proof. Admitted.
 
 Definition reject_impl_convert_from_parse_error  : reject :=
   (min) .+ (@repr WORDSIZE32 2).
@@ -35,10 +32,7 @@ Definition reject_impl_convert_from_parse_error  : reject :=
 Theorem ensures_reject_impl_convert_from_parse_error : forall result_1 ,
 @reject_impl_convert_from_parse_error  = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof.
-  intros ; subst.
-  reflexivity.
-Qed.
+Proof. Admitted.
 
 Inductive log_error :=
 | Full : log_error
@@ -53,13 +47,7 @@ Definition reject_impl_from_log_error (le_2 : log_error) : reject :=
 Theorem ensures_reject_impl_from_log_error : forall result_1 (le_2 : log_error),
 @reject_impl_from_log_error le_2 = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof.
-  intros ; subst.
-  unfold reject_impl_from_log_error.
-  destruct le_2.
-  - reflexivity.
-  - reflexivity.
-Qed.
+Proof. Admitted.
 
 Inductive new_contract_name_error :=
 | NewContractNameErrorMissingInitPrefix : new_contract_name_error
@@ -81,11 +69,7 @@ Theorem ensures_reject_impl_from_new_contract_name_error : forall result_1 (
   nre_3 : new_contract_name_error),
 @reject_impl_from_new_contract_name_error nre_3 = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof.
-  intros ; subst.
-  unfold reject_impl_from_new_contract_name_error.
-  destruct nre_3 ; reflexivity.
-Qed.
+Proof. Admitted.
 
 Inductive new_receive_name_error :=
 | NewReceiveNameErrorMissingDotSeparator : new_receive_name_error
@@ -105,17 +89,12 @@ Theorem ensures_reject_impl_from_new_receive_name_error : forall result_1 (
   nre_4 : new_receive_name_error),
 @reject_impl_from_new_receive_name_error nre_4 = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof.
-  intros ; subst.
-  unfold reject_impl_from_new_contract_name_error.
-  destruct nre_4 ; reflexivity.
-Qed.
+Proof. Admitted.
 
 Notation "'contract_state'" := (int32) : hacspec_scope.
 
-Inductive seek_result :=
-| SeekResultOk : int64 -> seek_result
-| SeekResultErr : unit -> seek_result.
+Notation "'seek_result'" := ((result (contract_state × int64
+  ) unit)) : hacspec_scope.
 
 Inductive seek_from :=
 | Start : int64 -> seek_from
@@ -129,160 +108,172 @@ Notation "'iint64_option'" := ((option int64)) : hacspec_scope.
 Definition contract_state_impl_seek
   (current_position_5 : contract_state)
   (pos_6 : seek_from)
-  : (contract_state × seek_result) :=
+  `{forall delta_7 ,
+  pos_6 = End (delta_7) ->
+  exists b_8 true}
+  : seek_result :=
   match pos_6 with
-  | Start offset_7 => (@cast _ uint32 _ (offset_7), SeekResultOk (offset_7))
-  | End delta_8 => (if ((delta_8) >=.? (@repr WORDSIZE64 0)):bool then (
+  | Start offset_9 => @Ok (contract_state × int64) unit ((
+      @cast _ uint32 _ (offset_9),
+      offset_9
+    ))
+  | End delta_10 => (if ((delta_10) >=.? (@repr WORDSIZE64 0)):bool then (
       match pub_uint32_checked_add (current_position_5) (@cast _ uint32 _ (
-          delta_8)) with
-      | Some b_9 => (b_9, SeekResultOk (@cast _ uint64 _ (delta_8)))
-      | None => (current_position_5, SeekResultErr (tt))
-      end) else (match pub_int64_checked_abs (delta_8) with
-      | Some b_10 => (
-        (@repr WORDSIZE32 4) .- (@cast _ uint32 _ (b_10)),
-        SeekResultOk (@cast _ uint64 _ ((@repr WORDSIZE32 4) .- (
-              @cast _ uint32 _ (b_10))))
-      )
-      | None => (current_position_5, SeekResultErr (tt))
+          delta_10)) with
+      | Some b_11 => @Ok (contract_state × int64) unit ((
+          b_11,
+          @cast _ uint64 _ (delta_10)
+        ))
+      | None => @Err (contract_state × int64) unit (tt)
+      end) else (match pub_int64_checked_abs (delta_10) with
+      | Some b_12 => @Ok (contract_state × int64) unit ((
+          (@repr WORDSIZE32 4) .- (@cast _ uint32 _ (b_12)),
+          @cast _ uint64 _ ((@repr WORDSIZE32 4) .- (@cast _ uint32 _ (b_12)))
+        ))
+      | None => @Err (contract_state × int64) unit (tt)
       end))
-  | Current delta_11 => (if ((delta_11) >=.? (@repr WORDSIZE64 0)):bool then (
+  | Current delta_13 => (if ((delta_13) >=.? (@repr WORDSIZE64 0)):bool then (
       match pub_uint32_checked_add (current_position_5) (@cast _ uint32 _ (
-          delta_11)) with
-      | Some offset_12 => (
-        offset_12,
-        SeekResultOk (@cast _ uint64 _ (offset_12))
-      )
-      | None => (current_position_5, SeekResultErr (tt))
-      end) else (match pub_int64_checked_abs (delta_11) with
-      | Some b_13 => match pub_uint32_checked_sub (current_position_5) (
-        @cast _ uint32 _ (b_13)) with
-      | Some offset_14 => (
-        offset_14,
-        SeekResultOk (@cast _ uint64 _ (offset_14))
-      )
-      | None => (current_position_5, SeekResultErr (tt))
+          delta_13)) with
+      | Some offset_14 => @Ok (contract_state × int64) unit ((
+          offset_14,
+          @cast _ uint64 _ (offset_14)
+        ))
+      | None => @Err (contract_state × int64) unit (tt)
+      end) else (match pub_int64_checked_abs (delta_13) with
+      | Some b_15 => match pub_uint32_checked_sub (current_position_5) (
+        @cast _ uint32 _ (b_15)) with
+      | Some offset_16 => @Ok (contract_state × int64) unit ((
+          offset_16,
+          @cast _ uint64 _ (offset_16)
+        ))
+      | None => @Err (contract_state × int64) unit (tt)
       end
-      | None => (current_position_5, SeekResultErr (tt))
+      | None => @Err (contract_state × int64) unit (tt)
       end))
   end.
 
 Definition load_state_hacspec
-  (buf_15 : public_byte_seq)
-  (offset_16 : int32)
-  : int32 :=
-  @repr WORDSIZE32 1.
+  (buf_17 : public_byte_seq)
+  (offset_18 : int32)
+  : (public_byte_seq × int32) :=
+  (buf_17, @repr WORDSIZE32 1).
+
+Theorem ensures_load_state_hacspec : forall result_1 (
+  buf_17 : public_byte_seq) (offset_18 : int32),
+@load_state_hacspec buf_17 offset_18 = result_1 ->
+(result_1) !=.? ((buf_17, @repr WORDSIZE32 2)).
+Proof. Admitted.
 
 Definition contract_state_impl_read_read
-  (current_position_17 : contract_state)
-  (buf_18 : public_byte_seq)
+  (current_position_19 : contract_state)
+  (buf_20 : public_byte_seq)
   : (contract_state × uint_size) :=
-  let num_read_19 : int32 :=
-    @repr WORDSIZE32 1 in
-  let num_read_19 :=
-    load_state_hacspec (buf_18) (current_position_17) in
-  ((current_position_17) .+ (num_read_19), @cast _ uint32 _ (num_read_19)).
+  let '(buf_21, num_read_22) :=
+    load_state_hacspec (buf_20) (current_position_19) in 
+  ((current_position_19) .+ (num_read_22), @cast _ uint32 _ (num_read_22)).
 
 Definition contract_state_impl_read_read_u64
-  (current_position_20 : contract_state)
-  (num_read_21 : int32)
+  (current_position_23 : contract_state)
+  (num_read_24 : int32)
   : (contract_state × bool) :=
   (
-    (current_position_20) .+ (num_read_21),
-    (num_read_21) =.? (@repr WORDSIZE32 8)
+    (current_position_23) .+ (num_read_24),
+    (num_read_24) =.? (@repr WORDSIZE32 8)
   ).
 
 Definition contract_state_impl_read_read_u32
-  (current_position_22 : contract_state)
-  (num_read_23 : int32)
+  (current_position_25 : contract_state)
+  (num_read_26 : int32)
   : (contract_state × bool) :=
   (
-    (current_position_22) .+ (num_read_23),
-    (num_read_23) =.? (@repr WORDSIZE32 4)
+    (current_position_25) .+ (num_read_26),
+    (num_read_26) =.? (@repr WORDSIZE32 4)
   ).
 
 Definition contract_state_impl_read_read_u8
-  (current_position_24 : contract_state)
-  (num_read_25 : int32)
+  (current_position_27 : contract_state)
+  (num_read_28 : int32)
   : (contract_state × bool) :=
   (
-    (current_position_24) .+ (num_read_25),
-    (num_read_25) =.? (@repr WORDSIZE32 1)
+    (current_position_27) .+ (num_read_28),
+    (num_read_28) =.? (@repr WORDSIZE32 1)
   ).
 
 Definition write_impl_for_contract_state_test
-  (current_position_26 : contract_state)
-  (len_27 : int32)
+  (current_position_29 : contract_state)
+  (len_30 : int32)
   : bool :=
-  option_is_none (pub_uint32_checked_add (current_position_26) (len_27)).
+  option_is_none (pub_uint32_checked_add (current_position_29) (len_30)).
 
 Definition write_impl_for_contract_state
-  (current_position_28 : contract_state)
-  (num_bytes_29 : int32)
+  (current_position_31 : contract_state)
+  (num_bytes_32 : int32)
   : (contract_state × uint_size) :=
-  ((current_position_28) .+ (num_bytes_29), @cast _ uint32 _ (num_bytes_29)).
+  ((current_position_31) .+ (num_bytes_32), @cast _ uint32 _ (num_bytes_32)).
 
 Definition has_contract_state_impl_for_contract_state_open  : contract_state :=
   @repr WORDSIZE32 0.
 
 Definition has_contract_state_impl_for_contract_state_reserve_0
-  (len_30 : int32)
-  (cur_size_31 : int32)
+  (len_33 : int32)
+  (cur_size_34 : int32)
   : bool :=
-  (cur_size_31) <.? (len_30).
+  (cur_size_34) <.? (len_33).
 
 Definition has_contract_state_impl_for_contract_state_reserve_1
-  (res_32 : int32)
+  (res_35 : int32)
   : bool :=
-  (res_32) =.? (@repr WORDSIZE32 1).
+  (res_35) =.? (@repr WORDSIZE32 1).
 
 Definition has_contract_state_impl_for_contract_state_truncate_0
-  (cur_size_33 : int32)
-  (new_size_34 : int32)
+  (cur_size_36 : int32)
+  (new_size_37 : int32)
   : bool :=
-  (cur_size_33) >.? (new_size_34).
+  (cur_size_36) >.? (new_size_37).
 
 Definition has_contract_state_impl_for_contract_state_truncate_1
-  (current_position_35 : contract_state)
-  (new_size_36 : int32)
+  (current_position_38 : contract_state)
+  (new_size_39 : int32)
   : contract_state :=
-  (if ((new_size_36) <.? (current_position_35)):bool then (new_size_36) else (
-      current_position_35)).
+  (if ((new_size_39) <.? (current_position_38)):bool then (new_size_39) else (
+      current_position_38)).
 
 Notation "'parameter'" := (int32) : hacspec_scope.
 
 Definition read_impl_for_parameter_read
-  (current_position_37 : parameter)
-  (num_read_38 : int32)
+  (current_position_40 : parameter)
+  (num_read_41 : int32)
   : (parameter × uint_size) :=
-  ((current_position_37) .+ (num_read_38), @cast _ uint32 _ (num_read_38)).
+  ((current_position_40) .+ (num_read_41), @cast _ uint32 _ (num_read_41)).
 
 Notation "'attributes_cursor'" := ((int32 × int16)) : hacspec_scope.
 
 Definition has_policy_impl_for_policy_attributes_cursor_next_test
-  (policy_attribute_items_39 : attributes_cursor)
+  (policy_attribute_items_42 : attributes_cursor)
   : bool :=
-  let '(_, remaining_items_40) :=
-    policy_attribute_items_39 in
-  (remaining_items_40) =.? (@repr WORDSIZE16 0).
+  let '(_, remaining_items_43) :=
+    policy_attribute_items_42 in 
+  (remaining_items_43) =.? (@repr WORDSIZE16 0).
 
 Definition has_policy_impl_for_policy_attributes_cursor_next_tag_invalid
-  (policy_attribute_items_41 : attributes_cursor)
-  (tag_value_len_1_42 : int8)
-  (num_read_43 : int32)
+  (policy_attribute_items_44 : attributes_cursor)
+  (tag_value_len_1_45 : int8)
+  (num_read_46 : int32)
   : (attributes_cursor × bool) :=
-  let '(current_position_44, remaining_items_45) :=
-    policy_attribute_items_41 in
-  let policy_attribute_items_46 : (int32 × int16) :=
-    ((current_position_44) .+ (num_read_43), remaining_items_45) in
-  (policy_attribute_items_46, (tag_value_len_1_42) >.? (@repr WORDSIZE8 31)).
+  let '(current_position_47, remaining_items_48) :=
+    policy_attribute_items_44 in 
+  let policy_attribute_items_49 : (int32 × int16) :=
+    ((current_position_47) .+ (num_read_46), remaining_items_48) in 
+  (policy_attribute_items_49, (tag_value_len_1_45) >.? (@repr WORDSIZE8 31)).
 
 Definition has_policy_impl_for_policy_attributes_cursor_next
-  (policy_attribute_items_47 : attributes_cursor)
-  (num_read_48 : int32)
+  (policy_attribute_items_50 : attributes_cursor)
+  (num_read_51 : int32)
   : attributes_cursor :=
-  let '(current_position_49, remaining_items_50) :=
-    policy_attribute_items_47 in
+  let '(current_position_52, remaining_items_53) :=
+    policy_attribute_items_50 in 
   (
-    (current_position_49) .+ (num_read_48),
-    (remaining_items_50) .- (@repr WORDSIZE16 1)
+    (current_position_52) .+ (num_read_51),
+    (remaining_items_53) .- (@repr WORDSIZE16 1)
   ).
