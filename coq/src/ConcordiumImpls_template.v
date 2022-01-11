ea90ed8ec7d2efcbc0f762463f3dc5788570fe76
Lasse Letager Hansen <lasse@letager.dk>
2022-01-11 08:38:17 +0100

    Trying to get auction and creusot to work with concordium impls in hacspec (WIP)

--- coq/src/ConcordiumImpls_template.v
+++ coq/src/ConcordiumImpls_template.v
@@ -22,7 +22,10 @@ Definition reject_impl_convert_from_unit  : reject_hacspec_t :=
 Theorem ensures_reject_impl_convert_from_unit : forall result_1 ,
 @reject_impl_convert_from_unit  = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof. Admitted.
+Proof.
+  intros ; subst.
+  reflexivity.
+Qed.
 
 Definition reject_impl_convert_from_parse_error  : reject_hacspec_t :=
   (min_v) .+ (@repr WORDSIZE32 2).
@@ -30,7 +33,10 @@ Definition reject_impl_convert_from_parse_error  : reject_hacspec_t :=
 Theorem ensures_reject_impl_convert_from_parse_error : forall result_1 ,
 @reject_impl_convert_from_parse_error  = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof. Admitted.
+Proof.
+  intros ; subst.
+  reflexivity.
+Qed.
 
 Inductive log_error_t :=
 | Full : log_error_t
@@ -46,7 +52,10 @@ Theorem ensures_reject_impl_from_log_error : forall result_1 (
   le_2 : log_error_t),
 @reject_impl_from_log_error le_2 = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof. Admitted.
+Proof.
+  intros ; subst.
+  destruct le_2 ; reflexivity.
+Qed.
 
 Inductive new_contract_name_error_t :=
 | NewContractNameErrorMissingInitPrefix : new_contract_name_error_t
@@ -68,7 +77,10 @@ Theorem ensures_reject_impl_from_new_contract_name_error : forall result_1 (
   nre_3 : new_contract_name_error_t),
 @reject_impl_from_new_contract_name_error nre_3 = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof. Admitted.
+Proof.
+  intros ; subst.
+  destruct nre_3 ; reflexivity.
+Qed.
 
 Inductive new_receive_name_error_t :=
 | NewReceiveNameErrorMissingDotSeparator : new_receive_name_error_t
@@ -88,7 +100,10 @@ Theorem ensures_reject_impl_from_new_receive_name_error : forall result_1 (
   nre_4 : new_receive_name_error_t),
 @reject_impl_from_new_receive_name_error nre_4 = result_1 ->
 (result_1) !=.? (@repr WORDSIZE32 0).
-Proof. Admitted.
+Proof.
+  intros ; subst.
+  destruct nre_4 ; reflexivity.
+Qed.
 
 Notation "'contract_state_hacspec_t'" := (int32) : hacspec_scope.
 
@@ -436,4 +451,3 @@ Definition combine_and_hacspec (l_89 : int32) (r_90 : int32) : int32 :=
 
 Definition combine_or_hacspec (l_91 : int32) (r_92 : int32) : int32 :=
   @repr WORDSIZE32 1.
-
