3f2b97b50abfce014dd75b42bf063c81eb291b76
Lasse Letager Hansen <lasse@letager.dk>
2022-01-20 18:51:45 +0100

    Updated tangle

--- coq/src/Concordium_Traits.v
+++ coq/src/Concordium_Traits.v
@@ -1,4 +1,4 @@
-(* [[file:concordium.org::*hacspec_concordium_traits - Coq code][hacspec_concordium_traits - Coq code:1]] *)
+(* [[file:concordium.org::* hacspec_concordium_traits - Coq code][hacspec_concordium_traits - Coq code:1]] *)
 (** This file was automatically generated using Hacspec **)
 Require Import Lib MachineIntegers.
 From Coq Require Import ZArith.
@@ -7,3 +7,4 @@ Open Scope Z_scope.
 Open Scope bool_scope.
 Open Scope hacspec_scope.
 (* hacspec_concordium_traits - Coq code:1 ends here *)
+
